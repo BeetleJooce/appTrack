269b18c8c1f2ac41eff8eb2c59e1103b1fc8e245
Mark Laffoon <mark.laffoon@k12northstar.org>
2011-01-05 08:21:22 -0900

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppTrack::Application.config.secret_token = 'f8d29af7f09435825cad20e3e0c1c28126119b9f56b5e8f915637ac93cdffc3d827e0832f3d0b0f3a27aa10de1a4537a37314e038d6baec67ea8c25e55e54418'
